77cec138604910ddfcee925594b1c117df2eadea
Jordi <jordi8101@gmail.com>
2022-09-22 09:07:21 -0700

    Added route container

---
+++ src/components/RouteContainer/index.js
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+export const RouteContainer = ({ children, isPrivate }) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  if (isPrivate && !userInfo) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
