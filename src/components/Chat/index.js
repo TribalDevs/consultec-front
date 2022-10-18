873b9e21230d7ad428159a3a1494858ffd51678f
Jordi <jordi8101@gmail.com>
2022-10-17 23:25:08 -0700

    Init chat component

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { ParagraphTextColor } from "utils";
 import { Message } from "components/Message";
 import "./styles.sass";
-import { PrimaryButton } from "components/Buttons";
+import { Button } from "components/Buttons";
+import { TextComponent } from "components/Texts";
+import { Form } from "components/Forms";
+import { RiSendPlaneFill } from "react-icons/ri";
 export const Chat = () => {
   return (
     <div className="chat__component">
       <div className="chat__header">
-        <ParagraphTextColor>Leonardo Mercado</ParagraphTextColor>
+        <TextComponent
+          type="h4"
+          text={{
+            en: "Walter White",
+            es: "Walter White",
+          }}
+        />
       </div>
       <div className="chat__history">
         <Message />
       </div>
       <div className="chat__input">
-        <form>
+        <Form>
           <input placeholder="Enviar mensaje" />
-          <PrimaryButton>
-            Enviar mensaje
-          </PrimaryButton>
-        </form>
+          <div className="chat__input__button">
+            <RiSendPlaneFill />
+          </div>
+        </Form>
       </div>
     </div>
   );
