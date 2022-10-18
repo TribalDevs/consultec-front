2f437141cd776e2eef53f5a51b3f6afa99a9d609
Jordi <jordi8101@gmail.com>
2022-10-17 23:25:16 -0700

    Init message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,23 @@
+import { TextComponent } from "components/Texts";
 import React from "react";
 import { ParagraphTextColor } from "utils";
 import "./styles.sass";
+const image =
+  "https://i.pinimg.com/originals/f1/85/6d/f1856d7b4f814ae073384f404054a793.jpg";
 export const Message = (props) => {
   return (
     <div className="message__container">
+      <div className="message__image">
+        <img src={image} alt="user" />
+      </div>
       <div className="message__content">
-        <ParagraphTextColor>Hola</ParagraphTextColor>
+        <TextComponent
+          type="p"
+          text={{
+            en: "Jesse jane no se mueve",
+            es: "Jesse jane no se mueve",
+          }}
+        />
         {/* <ParagraphTextColor className="message__date">
           {new Date()}
         </ParagraphTextColor> */}
