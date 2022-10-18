9cb6b79e0598207316a2907eb6a834cedf0d499c
Jordi <jordi8101@gmail.com>
2022-10-17 23:23:58 -0700

    Updated buttons

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,72 +1,42 @@
+// Styled components
 import styled from "styled-components";
+// Modifiers
 import { applyStyleModifiers } from "styled-components-modifiers";
+// utils
+import { typeScale, primaryFont } from "utils/texts";
+import { BUTTON_MODIFIERS } from "./modifiers";
+import { formatLanguageText } from "utils/texts";
 
-import { typeScale, primaryFont } from "utils";
+// icons
+// Loader icon
+import { BiLoaderAlt } from "react-icons/bi";
+// Error icon
+import { BiError } from "react-icons/bi";
 
-export const BUTTON_MODIFIERS = {
-  small: () => `
-      padding: 8px 1rem;
-      font-size: ${typeScale.paragraph};
-      `,
-  large: () => `
-      padding: 12px 20px;
-      font-size: ${typeScale.smallParagraph};
-  `,
-  form: () => `
-    padding: 10px 18px;
-    font-size: ${typeScale.smallParagraph};
-  `,
-  autoWidth: () => `
-    width: fit-content;
-  `,
-  delete: () => `
-    background-color: #ff0000;  
-    color: #fff;
-    border: none;
-    &:hover {
-      background-color: #f44336;
-      color: #fff;
-      border: none;
-    }
-  `,
-  formButton: () => `
-    margin-top: 0 !important;
-    `,
-};
-const Button = styled.button`
-  border: none;
-  padding: 12px 15px;
-  min-width: 110px;
-  font-size: 1rem;
-  line-height: 1rem;
-  border-radius: 2px;
-  transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out;
+// styles
+import styles from "./styles.module.sass";
+const ButtonBase = styled.button`
+  // Set font
   font-family: ${primaryFont};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.5rem;
-  &:hover {
-    cursor: pointer;
-  }
-  &:disabled {
-    cursor: not-allowed;
-    &:hover {
-      background-color: inherit;
-    }
-  }
 `;
-export const PrimaryButton = styled(Button)`
+const PrimaryButton = styled(ButtonBase)`
+  // Change background color between themes
   background-color: ${(props) => props.theme.buttons.primary.background};
+  // Change text color between themes
   color: ${(props) => props.theme.buttons.primary.color};
-  .loader__container__small {
-    border: 4px solid ${(props) => props.theme.buttons.primary.color};
+  i,
+  svg {
+    color: ${(props) => props.theme.buttons.primary.color};
   }
+  // Add border
+  border: 2px solid ${(props) => props.theme.buttons.primary.background};
+  // Hover
   &:hover {
     background-color: ${(props) =>
       props.theme.buttons.primary.hover.background};
     color: ${(props) => props.theme.buttons.primary.hover.color};
   }
+  // Disabled
   &:disabled {
     cursor: not-allowed;
     &:hover {
@@ -74,22 +44,37 @@ export const PrimaryButton = styled(Button)`
       color: ${(props) => props.theme.buttons.primary.color};
     }
   }
+  // Apply modifiers
   ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
-export const PrimaryButtonAlt = styled(PrimaryButton)`
+const PrimaryButtonAlt = styled(PrimaryButton)`
+  // Hover
   &:hover {
     color: ${(props) => props.theme.buttons.primary.hover.alt};
   }
 `;
-export const SecondaryButton = styled(Button)`
+const SecondaryButton = styled(ButtonBase)`
+  // Change background color between themes
   background-color: ${(props) => props.theme.buttons.secondary.background};
+  // Change text color between themes
   color: ${(props) => props.theme.buttons.secondary.color};
-
+  // Add border
+  border: 2px solid transparent;
+  i,
+  svg {
+    color: ${(props) => props.theme.buttons.secondary.color};
+  }
+  // Hover
   &:hover {
     background-color: ${(props) =>
       props.theme.buttons.secondary.hover.background};
     color: ${(props) => props.theme.buttons.secondary.hover.color};
+    i,
+    svg {
+      color: ${(props) => props.theme.buttons.secondary.hover.color};
+    }
   }
+  // Disabled
   &:disabled {
     cursor: not-allowed;
     &:hover {
@@ -97,17 +82,21 @@ export const SecondaryButton = styled(Button)`
       color: ${(props) => props.theme.buttons.secondary.color};
     }
   }
+  // Apply modifiers
   ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
-export const TertiaryButton = styled(Button)`
+const TertiaryButton = styled(ButtonBase)`
+  // Change background color between themes
   background-color: ${(props) => props.theme.buttons.tertiary.background};
+  // Change text color between themes
   color: ${(props) => props.theme.buttons.tertiary.color};
-
+  // Hover
   &:hover {
     background-color: ${(props) =>
       props.theme.buttons.tertiary.hover.background};
     color: ${(props) => props.theme.buttons.tertiary.hover.color};
   }
+  // Disabled
   &:disabled {
     cursor: not-allowed;
     &:hover {
@@ -115,18 +104,73 @@ export const TertiaryButton = styled(Button)`
       color: ${(props) => props.theme.buttons.tertiary.color};
     }
   }
+  // apply modifiers
   ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
-export const TogglerButton = styled(Button)`
-  transition: opacity 0.2s ease-in-out;
-  font-size: 0.7rem;
-  border-radius: 3px;
-  padding: 0.45rem 0.6rem;
-  box-shadow: 0 0.5rem 0.5rem rgba(0, 0, 0, 0.1);
-  min-width: 40px;
-
-  &:hover {
-    cursor: pointer;
-    opacity: 0.9;
-  }
+const FooterButton = styled(ButtonBase)`
+  // Change background color between themes
+  background: ${(props) => props.theme.buttons.secondary.background};
+  color: ${(props) => props.theme.buttons.secondary.color};
+  // Add border
+  border: none;
+  // Border radius
+  border-radius: 0;
+  // Font size
+  font-size: ${typeScale.helperText};
 `;
+
+export const Button = ({
+  type,
+  disabled,
+  onClick,
+  modifiers,
+  text,
+  language,
+  loading,
+  error,
+  ...props
+}) => {
+  // Get button type
+  const getButtonType = () => {
+    switch (type) {
+      case "primary":
+        return PrimaryButton;
+      case "primary-alt":
+        return PrimaryButtonAlt;
+      case "secondary":
+        return SecondaryButton;
+      case "tertiary":
+        return TertiaryButton;
+      case "footer":
+        return FooterButton;
+      default:
+        return PrimaryButton;
+    }
+  };
+  // Set button type
+  const ButtonType = getButtonType();
+  // Render
+  return (
+    <ButtonType
+      disabled={disabled}
+      onClick={onClick}
+      modifiers={modifiers}
+      {...props}
+      className={styles.button}
+    >
+      {loading ? (
+        <>
+          <BiLoaderAlt className={styles.loader} />
+          {language == "en" ? "Loading" : "Cargando"}
+        </>
+      ) : error && error.isError ? (
+        <>
+          <BiError className={styles.error} />{" "}
+          {formatLanguageText({ language, en: error.en, es: error.es })}
+        </>
+      ) : (
+        formatLanguageText({ language, en: text.en, es: text.es })
+      )}
+    </ButtonType>
+  );
+};
