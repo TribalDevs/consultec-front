61b92459e0d304ff4f08591f55b89237a2302c9a
Jordi <jordi8101@gmail.com>
2022-10-17 23:25:25 -0700

    Init sidebar chat

--- src/components/SidebarChat/index.js
+++ src/components/SidebarChat/index.js
@@ -1,22 +1,33 @@
+import { TextComponent } from "components/Texts";
 import React from "react";
 import { ParagraphTextColor } from "utils";
 import "./styles.sass";
 const users = [
   {
-    name: "Leonardo Mercado",
-    photo: "",
+    name: "Skyler White",
+    photo: "https://i.pinimg.com/originals/f1/85/6d/f1856d7b4f814ae073384f404054a793.jpg",
   },
   {
-    name: "Jazziel Bello",
-    photo: "",
+    name: "Walter White",
+    photo: "https://i.pinimg.com/originals/f1/85/6d/f1856d7b4f814ae073384f404054a793.jpg",
   },
+  {
+    name: "Jesse Pinkman",
+    photo: "https://i.pinimg.com/originals/f1/85/6d/f1856d7b4f814ae073384f404054a793.jpg",
+  }
 ];
 export const SidebarChat = () => {
   return (
     <div className="sidebar__chat">
       {users.map((user, index) => (
         <div className="user__item">
-          <ParagraphTextColor>{user.name}</ParagraphTextColor>
+          <TextComponent
+            type="h4"
+            text={{
+              en: user.name,
+              es: user.name,
+            }}
+          />
         </div>
       ))}
     </div>
