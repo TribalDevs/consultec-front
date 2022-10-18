66a9c2698dccf17063a0109a746df37c21bc3323
Jordi <jordi8101@gmail.com>
2022-10-17 23:23:48 -0700

    Created api function

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+export async function petition({
+  url,
+  method,
+  body,
+  constants,
+  token,
+  dispatch,
+  headers,
+  params,
+}) {
+  const { REQUEST, SUCCESS, FAILURE } = constants;
+  let petitionHeaders = {
+    "Content-Type": "application/json",
+  };
+  if (token) {
+    petitionHeaders["token"] = token;
+  }
+  let paramsPetition = {};
+  if (headers) {
+    petitionHeaders = { ...petitionHeaders, ...headers };
+  }
+  if (params) {
+    paramsPetition = params;
+  }
+  try {
+    dispatch({ type: REQUEST });
+    const { data } = await axios({
+      method,
+      url: process.env.REACT_APP_API_URL + url,
+      data: body,
+      headers: petitionHeaders,
+      params: paramsPetition,
+    });
+    dispatch({ type: SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: FAILURE,
+      payload: error.response?.data ? error.response.data : error.message,
+    });
+    if (
+      error.response?.data === "invalid token" ||
+      error.response?.data === "jwt expired"
+    ) {
+      localStorage.removeItem("token");
+    }
+    throw error.response?.data ? error.response.data : error.message;
+  }
+}
